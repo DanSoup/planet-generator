98c1b5e178eed929eea7bf6526a0353e8be638d8
DanSoup <dansoup@hotmail.co.uk>
2019-05-23 22:24:15 +0100

    added pixels to indicate the end of the poles

--- functions/drawPlanet.js
+++ functions/drawPlanet.js
@@ -1,5 +1,17 @@
 const drawPlanet = (canvas, planetData) => {
 
+  const poleAngle = []
+  
+  poleAngle[0] = Math.random() * Math.PI / 2;
+  poleAngle[1] = Math.random() * Math.PI;
+
+  const poleTips = []
+
+  poleTips[0] = [Math.cos(poleAngle[0]) * Math.cos(poleAngle[1]), Math.cos(poleAngle[0]) * Math.sin(poleAngle[1])];
+  poleTips[1] = [-poleTips[0][0], -poleTips[0][1]];
+
+
+
   const height = 200;
   const width = 200;
   const radius = 90;
@@ -18,12 +30,18 @@ const drawPlanet = (canvas, planetData) => {
 
   const noPixels = 200;
 
+  pixel(Math.round(poleTips[0][0] * radius + 100), Math.round(poleTips[0][1] * radius + 100));
+  pixel(Math.round(poleTips[1][0] * radius + 100), Math.round(poleTips[1][1] * radius + 100));
+
   for (let i = 0; i < noPixels; i++) {
     const x = Math.round(100 + radius * Math.sin((i / noPixels) * 2 * Math.PI))
     const y = Math.round(100 + radius * Math.cos((i / noPixels) * 2 * Math.PI))
     pixel(x, y)
   }
 
+
+
 };
 
 export default drawPlanet;
+
