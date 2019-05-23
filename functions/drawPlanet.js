dc3c70cca3133103cf4f4331b483dcf193203b13
Daniel Thomas <Daniel.Thomas@Weaveability.com>
2019-05-23 13:22:59 +0100

    drawing a black square

---
+++ functions/drawPlanet.js
@@ -0,0 +1,10 @@
+const drawPlanet = (canvas, planetData) => {
+
+  const ctx = mainCanvas.getContext('2d');
+
+  ctx.fillStyle = '#000000';
+  ctx.fillRect(0, 0, 200, 200)
+
+};
+
+export default drawPlanet;
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-import generatePlanet from './functions/generatePlanet.js/index.js';
+import generatePlanet from './functions/generatePlanet.js';
+import drawPlanet from './functions/drawPlanet.js';
 
 const planet = generatePlanet()
 
 console.log(planet)
 
 const mainCanvas = document.getElementById('mainCanvas')
+
+drawPlanet(mainCanvas, planet)
