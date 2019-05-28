388effe46f52687b365a7f6031dd3692bc546587
DanSoup <dansoup@hotmail.co.uk>
2019-05-28 22:18:36 +0100

    started hexCombiner

---
+++ functions/hexCombiner.js
@@ -0,0 +1,16 @@
+const hexCombiner = () => {
+
+  const colors = [...arguments];
+
+  let cR = 0;
+  let cG = 0;
+  let cB = 0;
+  let cA = 0;
+
+  colors.forEach(color => {
+
+  })
+
+};
+
+export default hexCombiner;
