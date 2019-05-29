17de7ef6fd67cf00953ebf3b116f8bdeda3a52ea
Daniel Thomas <Daniel.Thomas@Weaveability.com>
2019-05-29 15:43:42 +0100

    started work on the planet preview frontend for continued work

---
+++ pages/planetGenerator.js
@@ -0,0 +1,7 @@
+const xRot = document.getElementById('xRot').value;
+const yRot = document.getElementById('yRot').value;
+const zRot = document.getElementById('zRot').value;
+const radius = document.getElementById('radius').value;
+const nColor = document.getElementById('nColor').value;
+const eColor = document.getElementById('eColor').value;
+const sColor = document.getElementById('sColor').value;
